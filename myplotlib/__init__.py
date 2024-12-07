a48a23392d65b673e97550d61635e2a047d31ad5
Felix Molter <moltaire1@gmail.com>
2024-12-07 09:39:00 +0100

    automatically set defaults on import

--- myplotlib/__init__.py
+++ myplotlib/__init__.py
@@ -2,3 +2,6 @@ from .plots import *
 from .utilities import utilities
 from .utilities import annotation
 from .stats import *
+
+import matplotlib
+matplotlib = utilities.set_mpl_defaults(matplotlib)
